05573926c158029eb264a9c4a034c648fc5e9730
Konstantin Komelin <konstantin.komelin@gmail.com>
2017-12-11 22:41:57 +0300

    Working on the Service Worker.

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+
+(function() {
+    'use strict';
+
+    var cacheName = 'pwa-demo-v1';
+    var offlineImageURI = 'offline.png';
+    var filesToCache = [
+        '.',
+        'index.html',
+        'style.css',
+        'favicon.ico',
+        offlineImageURI
+    ];
+
+    self.addEventListener('install', function(event) {
+        event.waitUntil(
+            caches.open(cacheName).then(function(cache) {
+                return cache.addAll(filesToCache);
+            })
+        );
+    });
+
+    self.addEventListener('fetch', function(event) {
+
+        console.log('Fetch event for ', event.request.url);
+
+        var requestURL = new URL(event.request.url);
+
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                if (response) {
+                    console.log('Found ', event.request.url, ' in cache.');
+                    return response;
+                }
+                console.log('Network request for ', event.request.url);
+                return fetch(event.request);
+
+                // return response || fetch(event.request);
+            }).catch(function(error) {
+
+                if (requestURL.hostname === '2017.drupalyug.ru') {
+                    return caches.match(offlineImageURI);
+                }
+            })
+        );
+    });
+
+})();
